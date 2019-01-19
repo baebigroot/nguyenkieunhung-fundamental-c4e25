663e63d60f8f0ef9ffb73f2f3d762d6cf3560af1
unknown <kieunhung1229@gmail.com>
2019-01-19 17:49:42 +0700

    inital commit

---
+++ Lab3/homework/back-color-problem/back_color.py
@@ -0,0 +1,51 @@
+from random import *
+
+shapes = [
+    {
+        'text': 'blue',
+        'color': '#3F51B5',
+        'rect': [20, 60, 100, 100]
+    },
+    {
+        'text': 'red',
+        'color': '#C62828',
+        'rect': [140, 60, 100, 100]
+    },
+    {
+        'text': 'yellow',
+        'color': '#FFD600',
+        'rect': [20, 180, 100, 100]
+    },
+    {
+        'text': 'green',
+        'color': '#4CAF50',
+        'rect': [140, 180, 100, 100]
+    }
+]
+
+
+def get_shapes():
+    return shapes
+
+
+def generate_quiz():
+    return [
+                choice(["BLUE","RED","YELLOW","GREEN"]),
+                choice(["#3F51B5","#C62828","#FFD600","#4CAF50"]),
+                randint(0, 1)
+            ]
+def is_inside(x,y):
+    if(y[0]<x[0]<y[0]+y[2]) and (y[1]<x[1]<y[1]+y[3]):
+        return True
+    else:
+        return False
+
+def mouse_press(x, y, text, color, quiz_type):
+    for check in shapes:
+        if quiz_type == 1:
+            if check['text'].upper() == text:
+                click = is_inside([x, y], check['rect'])
+        else:
+            if check['color'] == color:
+                click = is_inside([x, y], check['rect'])
+    return click
